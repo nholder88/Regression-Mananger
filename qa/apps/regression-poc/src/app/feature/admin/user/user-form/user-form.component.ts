8552951e56ea0a66b6df7704eb1e6db2d2bd2020
Nigel M Holder <nholder88@gmail.com>
2019-12-15 01:28:46 -0500

    Move data pulls to service files

--- qa/apps/regression-poc/src/app/feature/admin/user/user-form/user-form.component.ts
+++ qa/apps/regression-poc/src/app/feature/admin/user/user-form/user-form.component.ts
@@ -1,6 +1,7 @@
 import {ChangeDetectionStrategy, Component, OnInit} from '@angular/core';
 import {UserService} from "../user.service";
 import {FormBuilder, Validators} from "@angular/forms";
+import {tap} from "rxjs/operators";
 
 @Component({
   selector: 'qa-user-form',
@@ -12,7 +13,9 @@ export class UserFormComponent implements OnInit {
 
   constructor(private userService: UserService, private formBuilder:FormBuilder) { }
 
-  user$= this.userService.selectedUser$;
+  user$= this.userService.selectedUser$.pipe(
+    tap( x=> console.log(x))
+  );
 
   userForm= this.formBuilder.group(
     {
@@ -24,8 +27,8 @@ export class UserFormComponent implements OnInit {
     }
   );
 
-  teamOptions=[{id:1, name:"Best squad"},{id:2, name:"ok  squad"},{id:3, name:"salt squad"} ];
-  roleOptions= [{id:1, name:"Admin"},{id:2, name:"QA"},{id:3, name:"Tester"}];
+  teamOptions$=this.userService.teams$;
+  roleOptions$= this.userService.userRoles$ ;
   ngOnInit() {
   }
 
--- qa/apps/regression-poc/src/app/feature/admin/user/user.service.ts
+++ qa/apps/regression-poc/src/app/feature/admin/user/user.service.ts
@@ -1,5 +1,8 @@
 import { Injectable } from '@angular/core';
-import {Observable, of} from 'rxjs';
+import {BehaviorSubject, combineLatest, Observable, of} from 'rxjs';
+
+import {shareReplay, tap} from "rxjs/operators";
+import {User} from "@qa/api-interfaces";
 
 @Injectable({
   providedIn: 'root'
@@ -9,7 +12,19 @@ export class UserService {
     { name: 'Duke', roles: ['admin', 'qa'], lastLogin: new Date(), team: 'All' }
   ]);
 
-  selectedUser$= of( { name: 'Duke', roles: ['admin', 'qa'], lastLogin: new Date(), team: 'All' });
+
+  userRoles$= of([{id:1, name:"Admin"},{id:2, name:"QA"},{id:3, name:"Tester"},{id:4, name:"Executive"}]);
+  teams$=of([{id:1, name:"Best squad"},{id:2, name:"Ok  squad"},{id:3, name:"Salt squad"},{id:4, name: "Fury Squad"} ]);
+
+  private selectedUserSubject= new BehaviorSubject<User>({id: 0, lastLogin: undefined, name: "", roles: [], team: ""});
+ userSelectedAction$= this.selectedUserSubject.asObservable();
 
   constructor() { }
+
+
+  selectedUser$= combineLatest([this.userSelectedAction$]).pipe(
+    tap(selectedUser=> console.log("User Selected",selectedUser)),
+    shareReplay(1)
+  );
+
 }
