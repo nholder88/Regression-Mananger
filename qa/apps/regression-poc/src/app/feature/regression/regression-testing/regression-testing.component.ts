fe4d720f133f5811af534d2e93f105358a07069f
Nigel Holder <nholder88@gmail.com>
2020-01-29 00:23:39 -0500

    Added new models Added faker

--- qa/apps/regression-poc/src/app/feature/regression/regression-testing/regression-testing.component.ts
+++ qa/apps/regression-poc/src/app/feature/regression/regression-testing/regression-testing.component.ts
@@ -1,6 +1,7 @@
-import { Component, OnInit } from '@angular/core';
-import { Regression, Test } from '@qa/api-interfaces';
-import { RegressionService } from '../regression.service';
+import {Component, OnInit} from '@angular/core';
+import {Regression, Test} from '@qa/api-interfaces';
+import {RegressionService} from '../regression.service';
+import * as faker from 'faker'
 
 @Component({
   selector: 'qa-regression-testing',
@@ -16,14 +17,14 @@ export class RegressionTestingComponent implements OnInit {
       name: 'Joe test',
       roles: [],
       lastLogin: null,
-      team: { id: 1, name: 'thesis' }
+      team: {id: 1, name: 'thesis'}
     },
     testCases: [{
       id: 0,
       caseStatus: '',
       testCase: {
         id: 10,
-        feature: { id: 0, name: 'Billing', enable: true, teams: [], subFeatures: [] },
+        feature: {id: 0, name: 'Billing', enable: true, teams: [], subFeatures: []},
         caseOrder: 1,
         description: 'Spicy jalapeno bacon ipsum dolor amet fatback excepteur salami do pork belly landjaeger ground round minim kevin anim ea alcatra nisi labore.'
       },
@@ -35,7 +36,7 @@ export class RegressionTestingComponent implements OnInit {
         caseStatus: '',
         testCase: {
           id: 10,
-          feature: { id: 0, name: 'Letters', enable: true, teams: [], subFeatures: [] },
+          feature: {id: 0, name: 'Letters', enable: true, teams: [], subFeatures: []},
           caseOrder: 12,
           description: 'Kevin pork belly pancetta, occaecat minim duis cupim tail shankle capicola alcatra bresaola sed exercitation dolor'
         },
@@ -47,7 +48,7 @@ export class RegressionTestingComponent implements OnInit {
         caseStatus: '',
         testCase: {
           id: 10,
-          feature: { id: 0, name: 'Letters', enable: true, teams: [], subFeatures: [] },
+          feature: {id: 0, name: 'Letters', enable: true, teams: [], subFeatures: []},
           caseOrder: 13,
           description: 'Commodo tempor minim labore, sirloin ut pariatur non sunt id ullamco dolore flank tri-tip.'
         },
@@ -59,7 +60,7 @@ export class RegressionTestingComponent implements OnInit {
         caseStatus: '',
         testCase: {
           id: 10,
-          feature: { id: 0, name: 'Letters', enable: true, teams: [], subFeatures: [] },
+          feature: {id: 0, name: 'Letters', enable: true, teams: [], subFeatures: []},
           caseOrder: 14,
           description: 'Meatloaf tongue voluptate short loin.'
         },
@@ -71,7 +72,7 @@ export class RegressionTestingComponent implements OnInit {
         caseStatus: '',
         testCase: {
           id: 10,
-          feature: { id: 0, name: 'Northwell Integration', enable: true, teams: [], subFeatures: [] },
+          feature: {id: 0, name: 'Northwell Integration', enable: true, teams: [], subFeatures: []},
           caseOrder: 15,
           description: 'Est corned beef tempor ham hock cillum pancetta irure sausage boudin hamburger ex incididunt.'
         },
@@ -83,7 +84,7 @@ export class RegressionTestingComponent implements OnInit {
         caseStatus: '',
         testCase: {
           id: 10,
-          feature: { id: 0, name: 'Northwell Integration', enable: true, teams: [], subFeatures: [] },
+          feature: {id: 0, name: 'Northwell Integration', enable: true, teams: [], subFeatures: []},
           caseOrder: 16,
           description: 'Pariatur magna voluptate landjaeger tri-tip. Minim sint proident ball tip buffalo.'
         },
@@ -95,7 +96,7 @@ export class RegressionTestingComponent implements OnInit {
         caseStatus: '',
         testCase: {
           id: 10,
-          feature: { id: 0, name: 'Northwell Integration', enable: true, teams: [], subFeatures: [] },
+          feature: {id: 0, name: 'Northwell Integration', enable: true, teams: [], subFeatures: []},
           caseOrder: 17,
           description: 'Ribeye deserunt filet mignon prosciutto.'
         },
@@ -107,7 +108,7 @@ export class RegressionTestingComponent implements OnInit {
         caseStatus: '',
         testCase: {
           id: 10,
-          feature: { id: 0, name: 'Other', enable: true, teams: [], subFeatures: [] },
+          feature: {id: 0, name: 'Other', enable: true, teams: [], subFeatures: []},
           caseOrder: 18,
           description: 'Cupim beef picanha nostrud tri-tip, ullamco et swine deserunt chislic cillum prosciutto.'
         },
@@ -129,9 +130,45 @@ export class RegressionTestingComponent implements OnInit {
     releaseName: 'Never'
   };
   regression$ = this.service.regressionWithAdd$;
+
+  tests:Array<Scenario> = [new Scenario(faker.lorem.slug(faker.random.number(35)),
+    faker.name.jobArea(), null, [new Steps(faker.lorem.sentences(3), faker.random.number(25)),
+      new Steps(faker.lorem.sentences(3), faker.random.number(25)),
+      new Steps(faker.lorem.sentences(3), faker.random.number(25))], new Date(), ''),
+    new Scenario(faker.lorem.slug(faker.random.number(35)),
+      faker.name.jobArea(), null, [], new Date(), ''),
+    new Scenario(faker.lorem.slug(faker.random.number(35)),
+      faker.name.jobArea(), null, [], new Date(), ''),
+    new Scenario(faker.lorem.slug(faker.random.number(35)),
+      faker.name.jobArea(), null, [new Steps(faker.lorem.sentences(3), faker.random.number(25)), new Steps(faker.lorem.sentences(3), faker.random.number(25)), new Steps(faker.lorem.sentences(3), faker.random.number(25))], new Date(), ''),
+  ];
+
   constructor(private service: RegressionService) {
   }
 
   ngOnInit() {
   }
+}
+
+export class Scenario {
+  constructor(
+    feature: string,
+    name: string,
+    result: boolean = false,
+    steps: Steps[] = [],
+    timestamp: Date = new Date(),
+    note: string = ''
+  ) {
+  }
+
+}
+
+export class Steps {
+  constructor(name: string, order: number) {
+
+  }
+
+  name: string;
+  order: number;
+
 }
