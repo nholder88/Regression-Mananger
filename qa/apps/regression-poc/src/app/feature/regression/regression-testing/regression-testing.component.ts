b774d9c72aaf3ed10e997363fa63f02c1177af9b
Nigel Holder <nholder88@gmail.com>
2020-02-11 23:42:35 -0500

    uodates for feature nav and save placholder

--- qa/apps/regression-poc/src/app/feature/regression/regression-testing/regression-testing.component.ts
+++ qa/apps/regression-poc/src/app/feature/regression/regression-testing/regression-testing.component.ts
@@ -2,11 +2,10 @@ import { Component, OnInit } from '@angular/core';
 
 import { RegressionService } from '../regression.service';
 
-import { Scenario } from '../models/scenario';
+import { Scenario, FeatureScenarioContainer } from '../models/scenario';
 import { ScenarioService } from '../scenario.service';
 import { Observable } from 'rxjs';
 import { TestPassService } from '../testpass.service';
-
 
 @Component({
   selector: 'qa-regression-testing',
@@ -14,19 +13,32 @@ import { TestPassService } from '../testpass.service';
   styleUrls: ['./regression-testing.component.css']
 })
 export class RegressionTestingComponent implements OnInit {
-
   regression$ = this.service.regressionWithAdd$;
-testPasses$=this.testPassService.testPasses$;
-  scenarios$: Observable<Array<Scenario>> = this.scenarioService.scenarioWithAdd$;
+  testPasses$ = this.testPassService.testPasses$;
+  scenarios$: Observable<Array<Scenario>> = this.scenarioService
+    .scenarioWithAdd$;
+  featureContainers: FeatureScenarioContainer[] = [
+    new FeatureScenarioContainer('Letters', []),
+    new FeatureScenarioContainer('Faxing', []),
+    new FeatureScenarioContainer('Meds', []),
+    new FeatureScenarioContainer('Shared Care', []),
+    new FeatureScenarioContainer('ASC', [])
+  ];
 
   constructor(
     private service: RegressionService,
     private scenarioService: ScenarioService,
-    private testPassService:TestPassService
+    private testPassService: TestPassService
   ) {}
 
-  ngOnInit() {
-
-
+  ngOnInit() {}
+  changeFeature() {
+    this.SaveScenarios();
+    this.scenarioService.savescenarioSubject.next(
+      this.scenarioService.createFakeScenario(1)[0]
+    );
+  }
+  SaveScenarios() {
+    console.log('Scenarios Saved.');
   }
 }
