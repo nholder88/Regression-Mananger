95f2d518e00d62e5a6e2d5c40155a6d72def8802
Nigel Holder <nholder88@gmail.com>
2020-04-11 21:50:52 -0400

    Update to refect the correct model
    Update to run API and UI easier.

--- qa/libs/api-interfaces/src/lib/regression/RegressionTesting.ts
+++ qa/libs/api-interfaces/src/lib/regression/RegressionTesting.ts
@@ -2,4 +2,10 @@ import { TestPass } from './TestPass';
 //Container for the passes and the root object
 export class RegressionTesting {
   constructor(public testPasses: TestPass[], public name: string) {}
+  id: number;
+  releaseName: string;
+  StartDate: Date;
+  EndDate: Date;
+  isComplete: boolean;
+  isStarted: boolean;
 }
