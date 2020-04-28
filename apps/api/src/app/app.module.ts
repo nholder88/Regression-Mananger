45a1a2b96d3659c45173ca9ffdd757fce34de644
Nigel Holder <nholder88@gmail.com>
2020-04-28 18:57:46 -0400

    Update app.module.ts

--- apps/api/src/app/app.module.ts
+++ apps/api/src/app/app.module.ts
@@ -12,11 +12,11 @@ import { AuthModule } from './auth/auth.module';
   imports: [
     TypeOrmModule.forRoot({
       type: 'mysql',
-      host: process.env.MYSQL_INSTANCE??'localhost',
-      port:  +process.env.MYSQL_INSTANCE_PORT  ?? 3306,
-      username: process.env.MYSQL_INSTANCE_USER ??'root',
-      password:process.env.MYSQL_INSTANCE_Password ??'root',
-      database:process.env.MYSQL_INSTANCE_DBNAME ??'test',
+      host: process.env.MYSQL_INSTANCE ?? 'localhost',
+      port: +process.env.MYSQL_INSTANCE_PORT ?? 3306,
+      username: process.env.MYSQL_INSTANCE_USER ?? 'root',
+      password: process.env.MYSQL_INSTANCE_PASSWORD ?? 'root',
+      database: process.env.MYSQL_INSTANCE_DBNAME ?? 'test',
       entities: getMetadataArgsStorage().tables.map(tbl => tbl.target),
       synchronize: true
     }),
