ef091ff9dbb78833dc4c1eb66ecefc0b0e0c4443
Nigel Holder <nholder88@gmail.com>
2020-05-03 22:20:40 -0400

    Revert "Merge branch 'develop' into feature/Header-UI-"
    This reverts commit ecc59c314a1c0b763263f1ef37a718dbddce97eb, reversing
    changes made to 03230c283afc586b45f33031765b25f4b5639e71.

---
+++ apps/regression-poc/src/app/feature/regression/components/regression-test-pass-form/regression-test-pass-form.component.ts
@@ -0,0 +1,62 @@
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { ClrWizard } from '@clr/angular';
+import { UserService } from '../../../admin/user/user.service';
+import { RegressionHeaderService } from '../../services/regression-header.service';
+import { TestPassService } from '../../services/testpass.service';
+import { FeatureService } from '../../services/feature.service';
+import { FeatureScenarioContainer, TestPass } from '@qa/api-interfaces';
+
+@Component({
+  selector: 'qa-regression-test-pass-form',
+  templateUrl: './regression-test-pass-form.component.html'
+
+})
+export class RegressionTestPassFormComponent implements OnInit {
+  constructor(
+    private formBuilder: FormBuilder,
+    private userService: UserService,
+    private testPassService: TestPassService,
+    private regressionService: RegressionHeaderService,
+    private featureService: FeatureService
+  ) {
+  }
+
+  // @ts-ignore
+  @ViewChild('wizardxl') wizardExtraLarge: ClrWizard;
+  xlOpen = false;
+  selectedFeatures = [];
+
+
+  testPassModel: TestPass = new TestPass([],
+    this.userService.getLoggedInUser(), new Date(), false, false);
+  testPassForm: FormGroup;
+  regressions$ = this.regressionService.regressionWithAdd$;
+  features$ = this.featureService.featureWithAdd$;
+  features: FeatureScenarioContainer[];
+
+  ngOnInit() {
+    this.features$.subscribe(x => this.features = x);
+    this.testPassForm = this.formBuilder.group(this.testPassModel);
+
+  }
+
+  onFinish() {
+    console.log(this.features);
+// Manually get the array and only add those that have been added
+    var featureScenarioContainers = this.features.map(feature => {
+      // Check if this has any scenarios to add
+      var scenarios= feature.scenarios.filter(s => s.enable);
+      var hasScenarios = scenarios.length> 0;
+            return hasScenarios? new FeatureScenarioContainer(feature.name,scenarios, feature.id ): null;
+    });
+
+    // Can probably use reduce here but want to make sure its valid first.
+    this.testPassForm.get('featureScenarioContainers').setValue(featureScenarioContainers.filter(x=> x));
+
+     this.testPassService.saveTestPass(this.testPassForm.value);
+    this.testPassForm.reset();
+    this.wizardExtraLarge.reset();
+  }
+
+}
