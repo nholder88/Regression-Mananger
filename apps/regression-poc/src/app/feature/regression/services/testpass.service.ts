4de69f6e0fb3f508c6d78f8fe5923d50b1e2b38d
Nigel Holder <nholder88@gmail.com>
2020-05-13 15:28:58 -0400

    Update testpass.service.ts

--- apps/regression-poc/src/app/feature/regression/services/testpass.service.ts
+++ apps/regression-poc/src/app/feature/regression/services/testpass.service.ts
@@ -14,58 +14,55 @@ export class TestPassService {
   constructor(
     private http: HttpClient,
     private errorHandler: ErrorHandlingService
-  ) {
-  }
+  ) {}
 
   rootUrl: string = `${environment.apiUrl}/TestPass`;
 
-  testPasses$ =
-    this.http.get<TestPass[]>(`${this.rootUrl}`).pipe(
-      catchError(this.errorHandler.handleError)
-    );
+  testPasses$ = this.http
+    .get<TestPass[]>(`${this.rootUrl}`)
+    .pipe(catchError(this.errorHandler.handleError));
 
   //TODO: Make this more robust to not have to hard code string...UGH
-  private testPassSelectedSubject = new BehaviorSubject<string>('');
+  private testPassSelectedSubject = new BehaviorSubject<TestPass>(null);
   testPassSelectedAction$ = this.testPassSelectedSubject.asObservable();
 
-
-  selectedTestPass$ = combineLatest([
-    this.testPasses$,
-    this.testPassSelectedAction$
-  ]).pipe(
-    map(([testPass, id]) => testPass.find(feat => feat.id === id))
-  );
-
-
-  // Change the selected Test Pass
-  selectedTestPassChanged(id: string): void {
-    this.testPassSelectedSubject.next(id);
-  }
+  selectedTestPass$ = this.testPassSelectedAction$;
 
   saveTestPassSubject = new Subject<TestPass>();
   testPassSavedAction$ = this.saveTestPassSubject.asObservable();
 
-  testPassesWithAdd$ = merge(
-    this.testPasses$,
-    this.testPassSavedAction$
-  ).pipe(
+  testPassesWithAdd$ = merge(this.testPasses$, this.testPassSavedAction$).pipe(
     scan((acc: TestPass[], value: TestPass) => [...acc, value]),
     catchError(err => this.errorHandler.handleError(err))
   );
+
+  // Change the selected Test Pass
+  selectedTestPassChanged(id: string): void {
+    // Get the test pass from the API with the test results and scenarios
+    if (id) {
+      // get the scenario results with this test pass
+      this.http
+        .get<TestPass>(
+          this.rootUrl + `/${id}?join=results&join=results.scenario`
+        )
+        .pipe(
+          // shareReplay(1, 150),
+          tap(x => console.log('Selected Test Pass Change retrieval', x))
+        )
+        .subscribe(testPass => this.testPassSelectedSubject.next(testPass));
+    }
+  }
 
   saveTestPass(testPass?: TestPass) {
     if (!testPass.id) {
       delete testPass.id;
     }
-    const saveObservable$ = testPass.id ? this.http
-      .put<TestPass>(this.rootUrl, testPass) : this.http
-      .post<TestPass>(this.rootUrl, testPass);
+    const saveObservable$ = testPass.id
+      ? this.http.put<TestPass>(this.rootUrl, testPass)
+      : this.http.post<TestPass>(this.rootUrl, testPass);
 
-    saveObservable$.pipe(
-      catchError(err => this.errorHandler.handleError(err)))
+    saveObservable$
+      .pipe(catchError(err => this.errorHandler.handleError(err)))
       .subscribe(x => this.saveTestPassSubject.next(x));
-
   }
-
-
 }
