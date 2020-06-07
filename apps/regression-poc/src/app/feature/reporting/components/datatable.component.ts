9934d691fab75dc1151f8716ddb61bc81d1f3591
Nigel Holder <nholder88@gmail.com>
2020-06-06 21:48:30 -0400

    Update datatable.component.ts

--- apps/regression-poc/src/app/feature/reporting/components/datatable.component.ts
+++ apps/regression-poc/src/app/feature/reporting/components/datatable.component.ts
@@ -5,51 +5,51 @@ import { ScenarioResult } from '@qa/api-interfaces';
   selector: 'qa-datatable',
   template: `
     <div class="clr-row">
-      <div class="clr-col">
+      <div class="clr-col-6">
         <div class="card">
           <div class="card-header">
-            Test Pass Results
+            Bar Graph
           </div>
           <div class="card-block">
             <div class="card-text">
-              <clr-datagrid>
-                <clr-dg-column [clrDgField]="'testPass.title'"
-                  >Test Pass</clr-dg-column
+              <div class="chart">
+                <canvas
+                  baseChart
+                  [data]="pieChartData"
+                  [labels]="pieChartLabels"
+                  [chartType]="pieChartType"
+                  [options]="pieChartOptions"
+                  [plugins]="pieChartPlugins"
+                  [colors]="pieChartColors"
+                  [legend]="pieChartLegend"
                 >
-                <clr-dg-column [clrDgField]="'scenario.name'"
-                  >Scenario</clr-dg-column
-                >
-                <clr-dg-column [clrDgField]="'timestamp'"
-                  >Test Date</clr-dg-column
-                >
-                <clr-dg-column [clrDgField]="'status'">Status</clr-dg-column>
-                <clr-dg-column [clrDgField]="'bugCreated'"
-                  >Bug Created?
-                </clr-dg-column>
+                </canvas>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
 
-                <clr-dg-row *clrDgItems="let result of gridData">
-                  <clr-dg-cell>{{ result.testPass.title }}</clr-dg-cell>
-                  <clr-dg-cell>{{ result.scenario.name }}</clr-dg-cell>
-                  <clr-dg-cell>{{
-                    result.timestamp | date: 'medium'
-                  }}</clr-dg-cell>
-                  <clr-dg-cell>{{ result.status }}</clr-dg-cell>
-                  <clr-dg-cell>
-                    <span *ngIf="result.bugCreated">Yes</span>
-                    <span *ngIf="!result.bugCreated">No</span>
-                  </clr-dg-cell>
-                </clr-dg-row>
-                <clr-dg-footer>
-                  <clr-dg-pagination #pagination [clrDgPageSize]="10">
-                    <clr-dg-page-size [clrPageSizeOptions]="[10, 20, 50, 100]"
-                      >Results per page</clr-dg-page-size
-                    >
-                    {{ pagination.firstItem + 1 }} -
-                    {{ pagination.lastItem + 1 }} of
-                    {{ pagination.totalItems }} results
-                  </clr-dg-pagination>
-                </clr-dg-footer>
-              </clr-datagrid>
+      <div class="clr-col-6">
+        <div class="card">
+          <div class="card-header">
+            Pie Chart
+          </div>
+          <div class="card-block">
+            <div class="card-text">
+              <div class="chart">
+                <canvas
+                  baseChart
+                  [data]="pieChartData"
+                  [labels]="pieChartLabels"
+                  [chartType]="'pie'"
+                  [options]="pieChartOptions"
+                  [plugins]="pieChartPlugins"
+                  [colors]="pieChartColors"
+                  [legend]="pieChartLegend"
+                >
+                </canvas>
+              </div>
             </div>
           </div>
         </div>
