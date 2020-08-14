f0c1190ca079201fc08aac81cc15eb4255d3fee2
Nigel Holder <nholder88@gmail.com>
2020-08-14 00:19:53 -0400

    Update to environment constants

--- apps/api/src/environments/environment.prod.ts
+++ apps/api/src/environments/environment.prod.ts
@@ -1,8 +1,8 @@
 export const environment = {
   production: true,
-  jwtConstants: { key: process.env.JWT_CONSTANT },
+  jwtConstants: { key: process.env.JWT_CONSTANTS_KEY },
   admin: {
     username: process.env.ADMIN_TEST_USER,
-    password: process.env.ADMIN_TEST_USERPASS
-  }
+    password: process.env.ADMIN_TEST_USERPASS,
+  },
 };
