d794c4b2c40a56d5b8d91e9e8aee2f6a9f9dfda5
Nigel Holder <nholder88@gmail.com>
2020-09-21 23:00:00 -0400

    admin separation

--- apps/regression-poc/src/app/feature/admin/components/scenario-listing.component.ts
+++ apps/regression-poc/src/app/feature/admin/components/scenario-listing.component.ts
@@ -1,65 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { ScenarioService } from '../../regression/services/scenario.service';
 import { Scenario } from '@qa/api-interfaces';
 
 @Component({
   selector: 'qa-scenario-listing',
-  template: `
-    <div class="clr-row">
-      <div class="clr-col-12">
-        <div class="card">
-          <div class="card-header">Scenarios</div>
-          <div class="card-block">
-            <div class="card-text">
-              <div class="clr-row">
-                <div class="clr-col-md">
-                  <clr-datagrid>
-                    <clr-dg-column [clrDgField]="'name'">Name</clr-dg-column>
-                    <clr-dg-column [clrDgField]="'feature.name'"
-                      >Feature</clr-dg-column
-                    >
-
-                    <clr-dg-column>Number of Steps</clr-dg-column>
-                    <clr-dg-column></clr-dg-column>
-                    <clr-dg-placeholder
-                      >We couldn't find any scenarios!
-                    </clr-dg-placeholder>
-                    <clr-dg-row *ngFor="let scenario of scenarios$ | async">
-                      <clr-dg-cell>{{ scenario.name }}</clr-dg-cell>
-                      <clr-dg-cell
-                        >{{ scenario?.feature?.name }} ({{
-                          scenario?.feature?.team
-                        }})
-                      </clr-dg-cell>
-
-                      <clr-dg-cell>{{ scenario.steps?.length }}</clr-dg-cell>
-                      <clr-dg-cell>
-                        <button
-                          class="btn btn-sm btn-outline"
-                          type="button"
-                          (click)="selectScenario(scenario)"
-                        >
-                          <clr-icon shape="pencil"></clr-icon>
-                          Edit
-                        </button>
-                        <button
-                          class="btn btn-sm btn-outline-danger"
-                          (click)="deleteScenario(scenario)"
-                        >
-                          <clr-icon shape="trash"></clr-icon>
-                          Remove
-                        </button>
-                      </clr-dg-cell>
-                    </clr-dg-row>
-                  </clr-datagrid>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-  `
+  templateUrl: 'scenario-listing.component.html'
 })
 export class ScenarioListingComponent implements OnInit {
   scenarios$ = this.scenarioService.modelWithDelete$;
--- apps/regression-poc/src/app/feature/admin/components/user-form.component.ts
+++ apps/regression-poc/src/app/feature/admin/components/user-form.component.ts
@@ -6,106 +6,32 @@ import { Roles } from '@qa/api-interfaces';
 
 @Component({
   selector: 'qa-user-form',
-  template: `
-    <div class="card" *ngIf="userForm$ | async as userForm">
-      <form
-        clrForm
-        clrLayout="horizontal"
-        [formGroup]="userForm"
-        (ngSubmit)="onSubmit(userForm)"
-      >
-        <div class="card-header">
-          <span *ngIf="!userForm.contains('id')">Add</span>
-          <span *ngIf="userForm.contains('id')">Edit</span>
-          User
-        </div>
-        <div class="card-block">
-          <div class="card-text">
-            <clr-input-container>
-              <label>User Name</label>
-              <input clrInput type="text" formControlName="username"/>
-              <clr-control-helper>Please enter the username</clr-control-helper>
-              <clr-control-error>Data is invalid</clr-control-error>
-            </clr-input-container>
-
-            <clr-input-container>
-              <label>Email </label>
-              <input clrInput type="text" formControlName="email"/>
-              <clr-control-helper
-              >Please enter the email address
-              </clr-control-helper
-              >
-              <clr-control-error>Data is invalid</clr-control-error>
-            </clr-input-container>
-
-            <clr-input-container *ngIf="userForm.contains('password')">
-              <label>Password</label>
-              <input clrInput type="password" formControlName="password"/>
-              <clr-control-helper></clr-control-helper>
-              <clr-control-error>Data is invalid</clr-control-error>
-            </clr-input-container>
-
-            <clr-select-container>
-              <label>Team</label>
-              <select clrSelect type="text">
-                <option value="-1">No Squad</option>
-                <option *ngFor="let team of teams$| async" [value]="team.id">{{
-                  team.name
-                  }}</option>
-              </select>
-              <clr-control-helper>Not Implemented</clr-control-helper>
-              <clr-control-error
-              >A team must be selected.
-              </clr-control-error>
-            </clr-select-container>
-            <clr-select-container>
-              <label>Role</label>
-              <select clrSelect type="text">
-                <option *ngFor="let role of roles$|async" [value]="role.id">
-                  {{ role.name }}</option
-                >
-              </select>
-
-              <clr-control-helper>Not Implemented</clr-control-helper>
-              <clr-control-error>This field is required!</clr-control-error>
-            </clr-select-container>
-          </div>
-          <button class="btn btn-sm btn-primary" type="submit">Save</button>
-          <button class="btn btn-sm btn-primary-outline" type="button" (click)="cancel()">Cancel</button>
-        </div>
-      </form>
-    </div>
-  `,
+  templateUrl: 'user-form.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class UserFormComponent {
   constructor(
     private userService: UserService,
     private formBuilder: FormBuilder
-  ) {
-  }
-
+  ) {}
 
   userForm$ = this.userService.selectedModel$.pipe(
     tap(u => console.log(u)),
     map(user => {
-      if (user)
-        return this.formBuilder.group(user
-        );
+      if (user) return this.formBuilder.group(user);
       else
         return this.formBuilder.group({
           username: ['', Validators.required],
           email: ['', Validators.required, Validators.email],
           password: ['', Validators.required]
         });
-      }
-    ),
-    tap(u => console.log(u)));
+    }),
+    tap(u => console.log(u))
+  );
 
   teams$ = this.userService.teams$;
   roles$ = this.userService.userRoles$;
   selectedRoles = new Array<Roles>();
-
 
   onSubmit(userForm) {
     this.userService.saveModel(userForm.value);
